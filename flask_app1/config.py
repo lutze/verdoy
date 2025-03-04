5e519ba57dd976f02d9bf5be8c565d8659c6126a
Matt <lutze@users.noreply.github.com>
2025-03-04 16:58:50 +0100

    Adding demo API

---
+++ flask_app1/config.py
@@ -0,0 +1,3 @@
+VERSION = "0.1"
+
+URL_ACTIVITIES = "https://httpbin.org/post"
---
+++ flask_app1/utils/utils.py
@@ -0,0 +1,23 @@
+import requests
+from flask import g
+
+def generic_api_requests(method, url, payload={}, params={}):
+    print("CURRENT REQUEST : ", method, url, payload)
+
+    try:
+        response = requests.request(
+            method,
+            url,
+            json=payload,
+            params=params,
+        )
+
+        json_response = response.json()
+
+        print( "RESPONSE SUCCESS")
+
+        return 1, json_response
+
+    except Exception as e:
+        print("RESPONSE ERROR :", e)
+        return 0, e
